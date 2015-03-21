077f652b1197af421f19867c53cb33fa33e06d26
Cari Westbrook <cari.westbrook@gmail.com>
2015-03-21 16:11:13 -0700

    start decoupling show page

---
+++ javascript/index.js
@@ -0,0 +1,54 @@
+var BASE_URL = "http://localhost:3000"
+
+var getAll = function(type) {
+  return $.ajax({
+    url: BASE_URL + type,
+    method: 'get',
+  });
+}
+
+var addQuoteToPage = function(quote) {
+  $('#quote').text(quote)
+}
+
+var addQuestionsToPage = function(data) {
+  var context = { questions: data };
+  var html = $('#question_template').html();
+  var questionTemplate = Handlebars.compile(html);
+  $('#all_questions').append(questionTemplate(context));
+}
+
+$(document).ready(function(){
+
+  var questionData = getAll('/questions');
+
+  questionData.done(function(response) {
+    addQuoteToPage(response.quote);
+    addQuestionsToPage(response.questions);
+  });
+
+ $(".new_question").on('submit', function(event){
+  event.preventDefault();
+
+  var formData = $(this).serialize();
+  var myUrl = BASE_URL + $(this).attr('action')
+  var myMethod = $(this).attr('method')
+
+  var questionRequest = $.ajax({
+    url: myUrl,
+    method: myMethod,
+    data: formData,
+  });
+
+  questionRequest.done(function(response){
+    var html = $('#question_template').html();
+    var questionTemplate = Handlebars.compile(html);
+    $('#all_questions').append(questionTemplate(response))
+  });
+
+  questionRequest.fail(function(response){
+    console.log("question request fails");
+  });
+
+ });
+});
